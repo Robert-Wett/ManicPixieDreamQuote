b1c29ea07eea1131c476241a6860b8790321ac57
Robert-Wett <rdwettlaufer@gmail.com>
2014-02-24 14:45:57 -0500

    Todo

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,18 @@
+/*
+var QBANK = {};
+QBANK.quotes = {};
+
+QBANK.loadNewQuote = function() {
+    var url = '/api/quote';
+    $.ajax(url).success(function(data) {
+        QBANK.quotes[data.id] = data.
+        $(".quoteHolder").html(data);
+    });
+};
+*/
 var loadNewQuote = function() {
-	var url = '/api/quote';
-	$.ajax(url).success(function(data) {
-		$(".quoteHolder").html(data);
-	});
+    var url = '/api/quote';
+    $.ajax(url).success(function(data) {
+        $(".quoteHolder").html(data);
+    });
 };
