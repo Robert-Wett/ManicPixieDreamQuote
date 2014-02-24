e2fd2d5c09557acfeda07bf28d14e5f0e4f2bbca
Robert-Wett <rdwettlaufer@gmail.com>
2014-02-24 14:46:54 -0500

    Adding :id endpoint

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-var express = require('express')
-,   app = express()
-,   server = require('http').createServer(app)
-,   path = require('path')
-,   _ = require('underscore')
-,   quoteFactory = require('./modules/quotes.js');
+var express = require('express'),
+    app = express(),
+    server = require('http').createServer(app),
+    path = require('path'),
+    _ = require('underscore'),
+    quoteFactory = require('./modules/quotes.js');
 
 var opts = {
     min: 1,
     max: 25
 };
 
-app.configure(function(){
+app.configure(function() {
     app.set('port', process.env.PORT || 5000);
     app.set('views', __dirname + '/views');
     app.set('view engine', 'ejs');
@@ -26,19 +26,33 @@ app.get('/', function(req, res) {
     var quote = quoteFactory.randomSet(opts.min, true);
     console.log(quote);
     //res.render('layout', {title: "Manic Pixie Dream Girl"});
-    res.render('index', {quote: quote, author: "-girl"});
+    res.render('index', {
+        quote: quote,
+        author: "-girl"
+    });
 });
 
-
+// Pull a random quote
 app.get('/api/quote', function(req, res) {
-    res.writeHead(200, { 'Content-Type': 'application/json', "Access-Control-Allow-Origin":"*" });
+    res.writeHead(200, {
+        'Content-Type': 'application/json',
+        "Access-Control-Allow-Origin": "*"
+    });
     res.write(JSON.stringify(quoteFactory.randomSet(opts.min, true)));
     res.end();
 });
 
+// Pull a specific quote
+app.get('/api/quote/:id', function(req, res) {
+        res.writeHead(200, {
+            'Content-Type': 'application/json',
+            "Access-Control-Allow-Origin": "*"
+        });
+        res.write(JSON.stringify(quoteFactory.getQuote(Number(id))); res.end();
+        });
 
 
-// Start it up
-server.listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
-});
+    // Start it up
+    server.listen(app.get('port'), function() {
+        console.log("Express server listening on port " + app.get('port'));
+    });
