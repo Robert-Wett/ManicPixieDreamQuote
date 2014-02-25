c844330af094f1a6bd630220458f94fb5cebb37d
Robert Wettlaufer <rdwettlaufer@gmail.com>
2014-02-25 00:30:49 -0500

    Tabs to spaces

--- test/test.quotes.js
+++ test/test.quotes.js
@@ -1,13 +1,13 @@
 var assert = require('assert')
-,	getRandomSet = require('../modules/quotes').getRandomSet
-,	getSpecificQuote = require('../modules/quotes').getSpecificQuote;
+,   getRandomSet = require('../modules/quotes').getRandomSet
+,   getSpecificQuote = require('../modules/quotes').getSpecificQuote;
 
 suite('getRandomSet', function() {
-	test('getRandomSet should return 20 entries', function() {
-		assertEqual(20, getRandomSet(20).length);
-	});
+    test('getRandomSet should return 20 entries', function() {
+        assertEqual(20, getRandomSet(20).length);
+    });
 
-	test('getRandomSet should return a single entry', function() {
-		assertEqual(1, getRandomSet(1, true).length);
-	});
+    test('getRandomSet should return a single entry', function() {
+        assertEqual(1, getRandomSet(1, true).length);
+    });
 });
