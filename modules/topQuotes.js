4fd40854269b5cc8b5a75b4cf8b862e6e171684f
Robert-Wett <rdwettlaufer@gmail.com>
2014-04-07 16:07:24 -0400

    updates/deletes

--- modules/topQuotes.js
+++
@@ -1,5 +0,0 @@
-/*
-    TODO:
-    Initial (non post-back/ajax) request should initialize with a curated list of the `best` quotes.
-    For the virality aspect of it, I want strong initial quotes to draw the user in.
- */
